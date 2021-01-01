576ce658a0d27fdf246503b2bc34c5d7bef1f8f7
Enrico Massone <enrico.massone@deltatre.it>
2021-01-01 22:00:35 +0100

    introduced the idea of use dropdown custom hook

---
+++ src/useDropdown.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const useDropdown = (label, defaultValue, options) => {
+  const [value, setValue] = useState(defaultValue);
+  const id = `use-dropdown-${label.toLowerCase().replace(" ", "")}`;
+
+  const Dropdown = () => (
+    <label htmlFor={id}>
+      {label}
+      <select
+        id={id}
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        onBlur={e => setValue(e.target.value)}
+      >
+        <option>All</option>
+        {options.map(item => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+
+  return [value, Dropdown, setValue];
+};
+
+export default useDropdown;
