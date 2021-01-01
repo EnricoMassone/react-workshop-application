fa2e2494ba37377566a85fe2415ff52daeeabfcc
Enrico Massone <enrico.massone@deltatre.it>
2021-01-01 21:24:56 +0100

    work in progress: adding search params form

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("San Francisco, CA");
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
@@ -14,6 +16,23 @@ const SearchParams = () => {
             value={location}
             onChange={e => setLocation(e.target.value)}
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onBlur={e => setAnimal(e.target.value)}
+            onChange={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
       </form>
     </div>
