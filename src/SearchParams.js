48ce439ead83bd0034b7caaafe21c81ba1ebfdab
Enrico Massone <enrico.massone@deltatre.it>
2021-01-01 21:50:30 +0100

    work in progress

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("San Francisco, CA");
   const [animal, setAnimal] = useState("dog");
+  const [breeds, setBreeds] = useState([]);
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    async function fetchBreeds() {
+      setBreeds([]);
+      setBreed("");
+
+      const { breeds: breedObjects } = await pet.breeds(animal);
+      const breedNames = breedObjects.map(({ name }) => name);
+      setBreeds(breedNames);
+    }
+
+    fetchBreeds().catch(console.error);
+  }, [animal]);
+
+  async function searchPets() {
+    const { animals } = await pet.animals({
+      location,
+      type: animal,
+      breed
+    });
+    setPets(animals || []);
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          searchPets().catch(console.error);
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -34,6 +64,26 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map(breed => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <button type="submit">Search</button>
       </form>
     </div>
   );
